fed398e2970611b4ce2e2dc915e3fac50a770b7f
Dana Wong <wangjie@DanadeMBP.lan>
2024-02-21 20:46:29 -0500

    assiament 1

---
+++ bool.py
@@ -0,0 +1,23 @@
+'''
+# You are given a triangle with a side #1 of 4, base of 6, and side #2 of 3. Create
+a brief python script that will determine the perimeter of the triangle. Comment your code
+
+1. Print the perimeter
+2. Using boolean operators is side #1 greater than the base?
+3. Using boolean operators is side #2 less than the base?
+4. Using boolean operators is base larger than or equal to side #1?
+
+'''
+
+
+perimeter = side_one + base +side_two 
+
+Variables 
+
+side_one =6 
+base =4
+side_two =3 
+
+# formula
+perimeter =side_one + base + side_two
+print(perimeter)
---
+++ variables_operators.py
@@ -0,0 +1,181 @@
+# print built in Function
+# greeting='Happy Wednesday '
+# print(greeting)
+
+# first_name ='Jie'
+# print(first_name)
+
+# Addition
+
+# print(4 + 2)
+# print(5 + 5)
+# print(100 +5)
+
+
+# Subtraction
+# print(10 - 3 )
+# print(4 -3 )
+# print(10 - 5 )
+
+
+
+
+# Multiplication
+
+
+
+# print(3 * 3)
+# print(2 * 8)
+# print(5 * 7)
+
+
+# Division
+
+# print(10 /2 )
+# print(5 /3 )
+# print(5 /0 )
+
+# try:
+#    print(5/0)
+# except ZeroDivisionError as e:
+#    print("you cannot divide by zero,have a great day")
+
+
+
+
+# Exponents
+
+# print(5 ** 5)
+# print(2 ** 5)
+# print(3 ** 6)
+
+
+# Integer Division
+# print(10 // 3)
+# print(4 // 3)
+# print(5// 2)
+
+
+# Modulo/Mod/Remainder
+# print (5 % 2)
+# print(10 % 4)
+# print(6 % 2)
+
+
+# Program to find the perimeter of a rectangle
+
+# perimeter = 2 *(length + width)
+
+# length = 10 
+# width = 7
+
+# perimeter = 2 *( length + width )
+# print ("The perimeter of this rectangle is",perimeter)
+
+
+# Data Types
+
+# Integer
+# num_one = 5 
+# print (num_one)
+# print(type(num_one))
+
+
+# String
+# fav_color ="blue"
+# print(fav_color)
+# print(type(fav_color))
+
+
+# Bool
+# technical_errors = True
+# print(technical_errors)
+# print(type(technical_errors))
+
+
+# Float
+# num_two = 1.22
+# print(num_two)
+# print(type(num_two))
+
+
+
+# String
+# first_name ="Dana"
+# last_name ="Wang"
+# fullname =first_name +' ' +last_name
+# print(first_name,last_name)
+
+
+# List 
+
+# student_grades =[100, 95 ,70, 85, 40]
+# print(student_grades)
+# print(type(student_grades))
+
+# For loop
+# for s in student_grades:
+#     print(s)
+
+# Dictionary
+# demographic_info ={"first name":"DANA",
+                  # "last name": "WANG",
+                  # "state":"New York"}
+# print(demographic_info)
+# print(type(demographic_info))
+
+# Cast a string to an integer
+# my_string = '5'
+# print(type(my_string))
+# new_number = int(my_string)
+# print(new_number)
+# print(type(new_number))
+
+
+# Colors 
+# fav_colors = ['blue','green','red','brown','black','purple']# list item has 4
+# num_colors = len (fav_colors)
+# print(num_colors)
+
+# color = 'orange'
+# my_count = len (color)
+# print(my_count)
+
+# for c in color:
+#     print (c)
+
+
+# Cast integer to string
+
+
+
+
+
+
+
+
+# Fahrenheit to Celsius
+
+# fahrenheit = 89
+# Celsius = (89 -32) * 5/9 
+# print(Celsius)
+
+# Eval
+
+# cold_weather ='True'
+# print(eval(cold_weather))
+
+
+
+
+
+side_one = 4
+base = 6
+side_two = 3
+
+perimeter = side_one + base +side_two
+
+print(perimeter)
+print( 'Using boolean operators is side #1 greater than the base?',side_one > base)
+print(side_two < base )
+print(base >= side_one)
