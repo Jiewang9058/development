2f4641e396860e5cc4bbcb54b60b6c128374ea6e
Dana Wong <wangjie@DanadeMBP.lan>
2024-04-23 02:23:42 -0400

    review

---
+++ Varia/class 12/class 15/functions1.py
@@ -0,0 +1,97 @@
+import math
+import name_module
+
+'''
+Exercise
+Write a function that will loop through a string and print whether a character is or is not a vowel.
+'''
+
+
+
+
+'''
+Example
+
+Write a function that returns the surface area of a box (rectangular prism).
+Surface Area = width*2 + length*2 + height*2
+
+'''
+
+
+'''
+Exercise
+Write a function that returns the surface area of a sphere.
+Surface Area = 4 * pi * radius^2
+
+'''
+
+
+
+''' Lets follow these functions through line by line and analyse the return statements'''
+
+# def get_vowels(word):
+#     out = ''
+#     vowels = 'aeiou'
+#     for w in word:
+#         if w in vowels:
+#             out += w
+    
+#     return out
+
+# my_word = 'bananas'
+
+# get_vowels(my_word)
+# print(get_vowels(my_word))
+
+# Return is none
+# def get_vowels(word):
+#     out = ''
+#     vowels = 'aeiou'
+#     for w in word:
+#         if w in vowels:
+#             out += w
+    
+#     print(out)
+
+# my_word = 'bananas'
+
+# get_vowels(my_word)
+# print(get_vowels(my_word)) # Return None
+
+
+
+'''
+Exercise
+Write a function that takes a list and a value, and removes the value until it no longer exists in the list.
+Return how many times the value was removed.
+'''
+
+sample_list = [4,5,3,2,4,3,3,3,3,2,2,1,1,1,1,0,5]
+value = 5
+
+
+
+'''
+Suppose you work for a bank, and you have a list of transactions with the following information for each one: customer ID, transaction amount, and transaction type (deposit or withdrawal).
+Write a function that takes in the list of customer transactions and returns a dictionary where the keys are the customer IDs and the values are the total transaction amounts for each customer.
+Example:
+transactions = [{'id': 'a', 'amount': 500, 'type': 'deposit'},\
+                {'id': 'b', 'amount': 350, 'type': 'deposit'},\
+                {'id': 'a', 'amount': 450, 'type': 'withdrawal'}]
+     Output: {'a': 50, 'b': 350}
+
+'''
+
+transactions = [{'id': 'a', 'amount': 500, 'type': 'deposit'},\
+                {'id': 'b', 'amount': 350, 'type': 'deposit'},\
+                {'id': 'a', 'amount': 450, 'type': 'withdrawal'},\
+                {'id': 'c', 'amount': 545, 'type': 'deposit'},\
+                {'id': 'c', 'amount': 225, 'type': 'withdrawal'},\
+                {'id': 'b', 'amount': 45, 'type': 'withdrawal'},\
+                {'id': 'e', 'amount': 150, 'type': 'deposit'}]
+
+
+
+
+''' Create a python file called name_module.py. Inside this python file, you will create 3 functions. One called full_name, another called reverse_name, and a third called get_initials. Each function will take 2 strings. One string will be the first name, the second string will be the last name. full_name will concatenate and return the full name. For example if the first string is "Joseph" and the second string is "Simpson" This function will return the string, "Joseph Simpson". Reverse name will flip the name around. The name Diana Prince, would come back as Prince Diana. The third function will take the first and last name and return the initials. The name Tony Stark will return T.S. Now create a second python file, called name.py. Import the module you just created and call the function with the necessary arguments so it prints a full names, reverse names, and initials as needed in the terminal'''
+
