f83a54da12202876ef66a30a89a41a307288e145
Dana Wong <wangjie@DanadeMBP.lan>
2024-05-09 23:09:04 -0400

    pro2updated

--- Varia/class 12/class 15/proj2_concepts_review.py
+++ Varia/class 12/class 15/proj2_concepts_review.py
@@ -24,52 +24,110 @@ from datetime import datetime
 
 
 # This is our fuction
-def find_even_number(my_list):
-    output_list = []
-    for m in my_list:
-        if m % 2 == 0:
-            output_list.append(m)
+# def find_even_number(my_list):
+#     output_list = []
+#     for m in my_list:
+#         if m % 2 == 0:
+#             output_list.append(m)
 
-    # print(output)
-    with open('even_nums.txt', 'w') as output:
-        for o in output_list:
-            o = str(o)
-            output.writelines(o)
-
-
-
-    print('File printed Successfully')
-
-my_list = [1,2,3,4,5,6,7,12,14,15,21,22] # this is the date we pass into our function 
+#     # print(output)
+#     with open('even_nums.txt', 'w') as output:
+#         for o in output_list:
+#             o = str(o)
+#             output.writelines(o)
 
 
-find_even_number(my_list) # this is our functione call 
+
+#     print('File printed Successfully')
+
+# my_list = [1,2,3,4,5,6,7,12,14,15,21,22] # this is the date we pass into our function 
+
+
+# find_even_number(my_list) # this is our functione call 
 
 
 ''' Lets read in the song lyrics and put it into a list, but before we do, lets look at other options we have to read files in'''
 
-with open('time_to_say_goodbye.txt','r') as lyrics:
-     my_paragrah = lyrics.read() # will read in everything
-     my_line = lyrics.readline()  # one line at a time 
-     lyrics_list = lyrics.readlines() # will deliver a list 
+# with open('time_to_say_goodbye.txt','r') as lyrics:
+#      my_paragrah = lyrics.read() # will read in everything
+#      my_line = lyrics.readline()  # one line at a time 
+#      lyrics_list = lyrics.readlines() # will deliver a list 
 
-# print(my_paragraph)
-print(my_line)
+# # print(my_paragraph)
+# print(my_line)    无法导入
+# print(lyrics_list)
 
 
 
 
 ''' Bank account class 
 Create the bank account class per the specifications in the powerpoint.  
+
+1.test after creating constructor 
+2.test string representation with print built in function on your object
+3. work through creating all of your methods 
 '''
 
 
-# ac_no_1 = BankAccount("Toninho Takeo", 2345, "05-05-24", 1000 )
+
+class BankAccount:
+    # Constructor
+    def __init__(self,customer_name: str, acct_num: int, date: str, balance:float):
+        self.customer_name = customer_name
+        self.acct_num = acct_num
+        self.date = date 
+        self.balance = balance
+
+ # string representation
+    def __str__(self):
+        return f'''Customer name: {self.customer_name}\nAccount number: {self.acct_num}\nDate of opening: {self.date}\nBalance: ${self.balance:.02f}'''
+    # Make a deposit, update the amount  
+    def deposit(self,amount):
+        self.balance += amount
+        print(f'{amount} has been deposited in your account')
+
+# Make a withdrawl ,update the amount 
+    def withdraw(self,amount):
+        if amount > self.balance:
+            print('Sorry,insufficient funds')
+        else:
+            self.balance -= amount
+            print(f'{amount} has been withdrawn in your account')
+    
+    def check_balance(self):
+        print(f'Your current balance is {self.balance}')
+
+
+    def days_since_opened(self):
+        open_date = datetime.strptime(self.date, '%m-%d-%y')
+        taday = datetime.now()
+        days_since_opened = taday - open_date
+        return f'Account opened {days_since_opened.days} days ago '
+
+    def print_customer_details(self):
+        f = open('customer_details.txt','w')
+        f.write(f'''Customer name: {self.customer_name}\nAccount number: {self.acct_num}\nDate of opening: {self.date}\nBalance: ${self.balance:.02f}\nAccount opened {self.days_since_opened()}''')
+        f.close()
+
+
+ac_no_1 = BankAccount("Toninho Takeo", 2345, "01-05-24", 1000.25 )
 # ac_no_2 = BankAccount("Jim Jones", 5424, "01-05-22", 1000 )
 # ac_no_3 = BankAccount("Sally Field", 3242, "11-04-15", 1000 )
 # ac_no_4 = BankAccount("Burt Reynolds", 4325, "08-11-13", 1000 )
 
+# string representation - what 
+# print(ac_no_1)
+# print(ac_no_2)
+# print(ac_no_3)
+# print(ac_no_4)
 
 
+# ac_no_1.check_balance()
+# ac_no_1.deposit(500)
 
+# ac_no_1.check_balance()
+# ac_no_1.withdraw(250)
+ac_no_1.print_customer_details()
+# ac_no_1.check_balance()
 
+print(ac_no_1.days_since_opened())
